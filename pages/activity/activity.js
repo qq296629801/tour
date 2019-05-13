d05ae99f9185007b8ae4902f989932360b9166c1
Eric <1932945408@qq.com>
2019-05-14 02:02:11 +0800

    动态模块页面开发完成

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -7,19 +7,129 @@ Page({
     sort: [],
     activitylist: [], //会议室列表列表
     scrolltop: null, //滚动位置
-    scrollview:0,
-    page: 1 ,
-    totalPage:null
+    scrollview: 0,
+    page: 1,
+    totalPage: null,
+    image_photo: '',
+    isOne: true,
+    list: [
+      {
+        id: '0',
+        avator: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: 'Eric',
+        con: '今天天气甚好，出来玩...',
+        imgs: [
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
+        ],
+        address: '长沙·浏阳市万花卡通园',
+        time: '50分钟前',
+        isLike: false
+      },
+      {
+        id: '1',
+        avator: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: 'Eric',
+        con: '今天天气甚好，出来玩...',
+        imgs: [
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
+        ],
+        address: '长沙·浏阳市万花卡通园',
+        time: '50分钟前',
+        isLike: true
+      },
+      {
+        id: '2',
+        avator: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: 'Eric',
+        con: '今天天气甚好，出来玩，今天天气甚好，出来玩今天天气甚好，出来玩今天天气甚好，出来玩今天天气甚好，出来玩今天天气甚好，出来玩今天天气甚好，出来玩',
+        imgs: [
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
+        ],
+        address: '长沙·浏阳市万花卡通园',
+        time: '50分钟前',
+        isLike: true
+      },
+      {
+        id: '3',
+        avator: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: 'Eric',
+        con: '今天天气甚好，出来玩...',
+        imgs: [
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+          'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
+        ],
+        address: '长沙·浏阳市万花卡通园',
+        time: '50分钟前',
+        isLike: false
+      }
+    ]
   },
-
+  selectType(e) {
+    this.setData({
+      isOne: e.currentTarget.dataset.index
+    })
+  },
+  //图片点击事件
+  imgYu (event) {
+    let src = event.currentTarget.dataset.src;//获取data-src
+    let imgList = event.currentTarget.dataset.list;//获取data-list
+    //图片预览
+    wx.previewImage({
+      current: src, // 当前显示图片的http链接
+      urls: imgList // 需要预览的图片http链接列表
+    })
+  },
+  choice() {
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        let imgs = res.tempFilePaths
+        for (let i = 0; i < imgs.length; i++) {
+          wx.uploadFile({
+            url: 'http://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+            filePath: imgs[i],
+            name: 'file',
+            formData: {
+              'user': 'test'
+            },
+            success: (res) => {
+              var data = res.data
+              //do something
+            }
+          })
+        }
+      }
+    })
+  },
   setList: function (page) {
     let that = this;
-    var data= wx.getStorageSync('tenderlist');
+    var data = wx.getStorageSync('tenderlist');
     console.log(data);
-    if(data!=null){
+    if (data != null) {
       this.setData(data)
     }
-    api.get(api.TENDER + "/list", { page: page})
+    api.get(api.TENDER + "/list", { page: page })
       .then(res => {
         var datas = res.data.data.list;
         for (var i in datas) {
@@ -77,14 +187,14 @@ Page({
       scrolltop: e.detail.scrollTop
     })
   },
-  searchScrollLower:function(){
-    let that=this;
-    var page = that.data.page+1;
+  searchScrollLower: function () {
+    let that = this;
+    var page = that.data.page + 1;
     wx.showLoading({ //期间为了显示效果可以添加一个过度的弹出框提示“加载中”  
       title: '加载中',
       icon: 'loading',
     });
-    if (page <= that.data.totalPage){
+    if (page <= that.data.totalPage) {
       api.get(api.TENDER + "/list", { page: page })
         .then(res => {
           var dataService = res.data.data.list;
@@ -99,7 +209,7 @@ Page({
           })
           wx.hideLoading();
         })
-    }else{
+    } else {
       wx.hideLoading();
     }
   },
